14cb247f0b4f9a10c3e94f351927e734075e5dce
SOJEONG KIM <thanku1598@gmail.com>
2021-08-17 23:31:17 +0900

    210817-2

---
+++ chapter14/src/sec05/exam01_runnable/RunnalbleExample.java
@@ -0,0 +1,28 @@
+package sec05.exam01_runnable;
+
+public class RunnalbleExample {
+
+	public static void main(String[] args) {
+		
+/*		Runnable runnable = () -> {
+*			for(int i=0; i<10; i++) {
+*				System.out.println(i);
+*			}
+*		};
+*		
+*		Thread thread = new Thread(runnable);
+*		thread.start();
+*/
+
+		
+		//주로 이 방법 사
+		Thread thread = new Thread(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.println(i);
+			}
+		});
+		thread.start();
+
+	}
+
+}
