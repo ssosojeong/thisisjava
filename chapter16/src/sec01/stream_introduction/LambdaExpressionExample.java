013dbd175cedcfb35f44e77b239946c50669e7cb
SOJEONG KIM <thanku1598@gmail.com>
2021-08-22 23:51:17 +0900

    120822

--- chapter16/src/sec01/stream_introduction/LambdaExpressionExample.java
+++ chapter16/src/sec01/stream_introduction/LambdaExpressionExample.java
@@ -15,7 +15,7 @@ public class LambdaExpressionExample {
 		stream.forEach( s -> {
 			String name = s.getName();
 			int score = s.getScore();
-			System.out.println(name + ": " + score); //
+			System.out.println(name + ": " + score); //ㅎ
 		});
 				
 		
